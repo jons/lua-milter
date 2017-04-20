944d6260d6ddae2866662565710d235664a231d0
jons <jon@wroth.org>
2017-04-20 19:49:44 +0000

    add increment examples to test.lua
      just a sanity check sort of demo that proves the envelope state
      persists across callbacks

--- test.lua
+++ test.lua
@@ -3,17 +3,27 @@ function negotiate (envelope, f0, f1, f2, f3)
   r = Milter.setsymlist(envelope, Milter.SMFIM_HELO, "{client_addr}")
   ret = (Milter.MI_SUCCESS == r and "OK" or "failed")
   print("setsymlist: " .. ret)
+  envelope.headers = 0
+  envelope.bytes = 0
   return Milter.SMFIS_ALL_OPTS
 end
 
-function helo (envelope)
-  addr = Milter.getsymval(envelope, "{client_addr}")
+function helo (envelope, addr)
+  --addr = Milter.getsymval(envelope, "{client_addr}")
   print("[" .. envelope.sid .. "] helo " .. addr)
   return Milter.SMFIS_CONTINUE
 end
 
+function header (envelope, name, value)
+  envelope.headers = envelope.headers + 1
+end
+
+function data (envelope, segment, len)
+  envelope.bytes = envelope.bytes + len
+end
+
 function close (envelope)
-  print("[" .. envelope.sid .. "] close")
+  print("[" .. envelope.sid .. "] close (" .. envelope.headers .. " headers, " .. envelope.bytes .. " bytes)")
   return Milter.SMFIS_CONTINUE
 end
 
@@ -22,6 +32,8 @@ milter:setConnection("inet:12345")
 milter:setFlags(0)
 milter:setNegotiate(negotiate)
 milter:setHELO(helo)
+milter:setHeader(header)
+milter:setBody(data)
 milter:setClose(close)
 print(Milter.SMFIS_CONTINUE, "continue")
 print(Milter.SMFIS_REJECT, "reject")
